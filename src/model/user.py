b033e3e78e9d3eaef9cbba6a25dd6a58e947a1bb
calm1205 <nagi.moriyama@mntsq.com>
2025-12-08 22:51:51 +0900

    fix: str -> uuid

--- src/model/user.py
+++ src/model/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 import uuid6
 from src.repository.database import Base
@@ -7,7 +8,7 @@ from src.repository.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    uuid = Column(String(32), primary_key=True, default=lambda: uuid6.uuid7().hex)
+    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid6.uuid7)
     name = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
     updated_at = Column(
