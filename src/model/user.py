c5df1fc034805b8abc9f4c3ea893cd5bbe33ca68
calm1205 <nagi.moriyama@mntsq.com>
2025-12-08 22:53:30 +0900

    fix: rm utcnow

--- src/model/user.py
+++ src/model/user.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.dialects.postgresql import UUID
-from datetime import datetime
+from datetime import datetime, timezone
 import uuid6
 from src.repository.database import Base
 
@@ -10,7 +10,7 @@ class User(Base):
 
     uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid6.uuid7)
     name = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at = Column(
-        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False
+        DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False
     )
