3a3dffcb81545ea6c4de5d712b5d115edcad8ad6
calm1205 <authentic.nagi@gmail.com>
2026-02-08 09:21:52 +0900

    feat: usersテーブル削除のマイグレーションを追加

---
+++ alembic/versions/745592fe5c87_drop_users_table.py
@@ -0,0 +1,42 @@
+"""drop users table
+
+Revision ID: 745592fe5c87
+Revises: 318327bd505d
+Create Date: 2026-02-08
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '745592fe5c87'
+down_revision: Union[str, Sequence[str]] = '318327bd505d'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.drop_constraint('expenses_user_uuid_fkey', 'expenses', type_='foreignkey')
+    op.drop_column('expenses', 'user_uuid')
+    op.drop_constraint('categories_user_uuid_fkey', 'categories', type_='foreignkey')
+    op.drop_column('categories', 'user_uuid')
+    op.drop_table('users')
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.create_table('users',
+        sa.Column('uuid', sa.String(length=32), nullable=False),
+        sa.Column('name', sa.String(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.PrimaryKeyConstraint('uuid')
+    )
+    op.add_column('categories', sa.Column('user_uuid', sa.String(length=32), nullable=False))
+    op.create_foreign_key('categories_user_uuid_fkey', 'categories', 'users', ['user_uuid'], ['uuid'])
+    op.add_column('expenses', sa.Column('user_uuid', sa.String(length=32), nullable=False))
+    op.create_foreign_key('expenses_user_uuid_fkey', 'expenses', 'users', ['user_uuid'], ['uuid'])
