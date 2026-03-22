09d8c87a5e6d4f4c0cedadb82c23968a02e4094c
calm1205 <authentic.nagi@gmail.com>
2026-03-22 17:20:29 +0900

    feat: @themeでテーマカラーを定義し共通カラー定数を追加

---
+++ frontend/src/lib/colors.ts
@@ -0,0 +1,19 @@
+export const CHART_COLORS = [
+  "var(--color-chart-1)",
+  "var(--color-chart-2)",
+  "var(--color-chart-3)",
+  "var(--color-chart-4)",
+  "var(--color-chart-5)",
+  "var(--color-chart-6)",
+  "var(--color-chart-7)",
+  "var(--color-chart-8)",
+]
+
+export const RANGE_COLORS = [
+  "var(--color-chart-5)",
+  "var(--color-chart-4)",
+  "var(--color-chart-3)",
+  "var(--color-chart-2)",
+  "var(--color-chart-1)",
+  "var(--color-chart-8)",
+]
