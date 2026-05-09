22559fffcd534db816101c63ecc30b7dcd491f04
calm1205 <nagi.moriyama@nagi-2.local>
2026-05-09 20:48:40 +0900

    feat: RecurringExpenseモデルとマイグレーション追加
    - src/model/recurring_expense.py: RecurringExpense + IntervalUnit
    - src/model/expense.py: recurring_expense_uuid (FK, ON DELETE SET NULL) 追加
    - src/model/__init__.py: 新モデルexport
    - alembic versions: recurring_expenses作成 + expenses列追加
    
    issue: #81

--- backend/src/model/__init__.py
+++ backend/src/model/__init__.py
@@ -2,6 +2,7 @@ from src.model.category import Category
 from src.model.expense import Expense
 from src.model.expense_category_association import ExpenseCategoryAssociation
 from src.model.expense_template import ExpenseTemplate
+from src.model.recurring_expense import IntervalUnit, RecurringExpense
 from src.model.user import User
 from src.model.utils import generate_uuid_string
 from src.model.webauthn_credential import WebAuthnCredential
@@ -11,6 +12,8 @@ __all__ = [
     "Expense",
     "ExpenseCategoryAssociation",
     "ExpenseTemplate",
+    "IntervalUnit",
+    "RecurringExpense",
     "User",
     "WebAuthnCredential",
     "generate_uuid_string",
